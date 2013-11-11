1797216ddd6f6bbdb22bd155b5883021cabef944
Marc Leglise <mleglise@gmail.com>
2013-11-11 08:24:43 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChiliCookoffVoter::Application.config.secret_key_base = 'aca894d640c127eb88697140433afa86431c8f3032d55b6798f68e6dfbae007bb89487becc4238cb39d2bef5064910690ce28e0b02d2b28b617974297c368b24'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChiliCookoffVoter::Application.config.session_store :cookie_store, key: '_chili-cookoff-voter_session'
