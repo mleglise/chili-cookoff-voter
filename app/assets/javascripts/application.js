dbef8d3700c46f516098adc6f4e2df8c7620b849
Marc Leglise <mleglise@gmail.com>
2015-10-10 11:22:37 -0700

    Events#show: added countdown timer to polls closing.
    - Hide voting until polls are open.
    - Hide everything after polls are closed.
    - Refresh the page when countdown finishes, to close the polls.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require jquery.rateit
+//= require countdown
 //= require_tree .
--- app/assets/javascripts/events_controller.js
+++ app/assets/javascripts/events_controller.js
@@ -1,3 +1,25 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-// You can use CoffeeScript in this file: http://coffeescript.org/
+
+(function(window,undefined){
+  "use strict";
+
+  // Sale countdown
+  if( $("#countdown_alert").length > 0 ){
+    var endtime = new Date( $('#countdown_text').data('endtime') ),
+        timerId = countdown(
+          function(ts) {
+            var new_time_left = ts.toHTML('strong');
+            if( new_time_left === '' ){
+              // Timer has run out.
+              window.location.reload();
+            } else {
+              document.getElementById('countdown_text').innerHTML = new_time_left;
+            }
+          },
+          endtime,
+          countdown.DAYS|countdown.HOURS|countdown.MINUTES);
+    $('#countdown_alert').show();
+  }
+
+})(window);
